a04144c03b4092a6704f0cc433a17166f072abc4
thejsj <jorge.silva@thejsj.com>
2014-02-02 23:21:11 -0500

    Adding Minified Footer and Jquery

--- functions/queue-scripts.php
+++ functions/queue-scripts.php
@@ -8,19 +8,18 @@ add_action('login_head', 'custom_login_styles');
 
 // Load theme scripts
 function load_theme_scripts() {
-	if(!LOAD_ONLY_DOM){
+	//if(!LOAD_ONLY_DOM){ }
 
-		// Load Header Script
-		wp_register_script('header', get_template_directory_uri()  . '/static/js/header.js', array(), '', false);
-		wp_enqueue_script('header');
+	// Load Header Script
+	wp_register_script('header', get_template_directory_uri()  . '/static/js/header.js', array(), '', false);
+	wp_enqueue_script('header');
 
-		// deregister_scripts();
+	// deregister_scripts();
 
-		// Load Footer Script
-		wp_register_script('jquery', get_template_directory_uri()  . '/static/js/footer.js', array(), '', true);
-		wp_enqueue_script('jquery');
-
-	}		
+	// Load Footer Script
+	wp_deregister_script( 'jquery' );
+	wp_register_script('jquery', get_template_directory_uri()  . '/static/js/footer.js', array(), '', true);
+	wp_enqueue_script('jquery');	
 }
 
 add_action( 'wp_enqueue_scripts', 'load_theme_scripts', 2 );
