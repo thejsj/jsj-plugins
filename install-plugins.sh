4034f42bc3739e30c7d5d5411223beed00c821eb
thejsj <jorge.silva@thejsj.com>
2014-08-30 16:24:53 -0400

    Adding install-plugins bash script

---
+++ install-plugins.sh
@@ -0,0 +1,9 @@
+cd ../../plugins/
+
+rm -rf jsj-gs-bravo
+rm -rf jsj-gs-moa
+rm -rf jsj-gs-volga
+
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-bravo.zip -d ./
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-moa.zip -d ./
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-volga.zip -d ./
