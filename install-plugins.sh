cc5a5d10ebf8b4464add1899b5933e709a6e1015
thejsj <jorge.silva@thejsj.com>
2014-08-30 16:27:43 -0400

    Adding replace all flag

--- install-plugins.sh
+++ install-plugins.sh
@@ -4,6 +4,6 @@ rm -rf jsj-gs-bravo
 rm -rf jsj-gs-moa
 rm -rf jsj-gs-volga
 
-unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-bravo.zip -d ./
-unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-moa.zip -d ./
-unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-volga.zip -d ./
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-bravo.zip -A -d ./
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-moa.zip -A -d ./
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-volga.zip -A -d ./
