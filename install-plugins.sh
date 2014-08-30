9d6d12a59578a20d4ffe75cad3b203227705532f
thejsj <jorge.silva@thejsj.com>
2014-08-30 16:30:27 -0400

    Removing A tag

--- install-plugins.sh
+++ install-plugins.sh
@@ -4,6 +4,6 @@ rm -rf jsj-gs-bravo/**/*
 rm -rf jsj-gs-moa/**/*
 rm -rf jsj-gs-volga/**/*
 
-unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-bravo.zip -A -d ./jsj-gs-bravo
-unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-moa.zip -A -d ./jsj-gs-moa
-unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-volga.zip -A -d ./jsj-gs-volga
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-bravo.zip -d ./jsj-gs-bravo
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-moa.zip -d ./jsj-gs-moa
+unzip /var/www/thejsj.com/public_html/2014/jsj-plugins-repo/jsj-gs-volga.zip -d ./jsj-gs-volga
